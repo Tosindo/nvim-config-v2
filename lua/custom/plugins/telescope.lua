a250d83afc7c901c548237ea7fe52e775fb467da
Ivo Silva <ivomspt24@gmail.com>
2024-09-13 02:28:00 +0100

    add smart open

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -102,4 +102,23 @@ return {
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
     end, { desc = '[S]earch [N]eovim files' })
   end,
+  {
+    'danielfalk/smart-open.nvim',
+    branch = '0.2.x',
+    config = function()
+      require('telescope').load_extension 'smart_open'
+
+      -- for now bind to ;ss
+      vim.keymap.set('n', ';ss', function()
+        require('telescope').extensions.smart_open.smart_open()
+      end, { desc = '[S]earch [S]mart Open' })
+    end,
+    dependencies = {
+      'kkharji/sqlite.lua',
+      -- Only required if using match_algorithm fzf
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+      { 'nvim-telescope/telescope-fzy-native.nvim' },
+    },
+  },
 }
