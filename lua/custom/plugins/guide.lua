88949a7637bb80b3f08a7dad5c8cad0a4d250252
Ivo Silva <ivomspt24@gmail.com>
2024-09-14 23:26:21 +0100

    more polishing

---
+++ lua/custom/plugins/guide.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'tris203/precognition.nvim',
+    event = 'VeryLazy',
+    opts = {
+      highlightColor = {
+        link = 'Whitespace',
+      },
+    },
+  },
+}
--- lua/custom/plugins/statusline.lua
+++ lua/custom/plugins/statusline.lua
@@ -1,4 +1,6 @@
 return {
+  -- look into https://github.com/OXY2DEV/bars-N-lines.nvim
+  -- seems like an interesting and beautiful implementation
   {
     dir = vim.fn.stdpath 'config' .. '/lua/tosindo/statusline',
     name = 'tosindo-statusline',
