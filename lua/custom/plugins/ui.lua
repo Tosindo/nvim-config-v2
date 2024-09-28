c9926ea7caefcff2ee93812ffd32336d2e410d09
Ivo Silva <ivomspt24@gmail.com>
2024-09-28 22:52:04 +0100

    add image support

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -130,4 +130,24 @@ return {
       }
     end,
   },
+  {
+    '3rd/image.nvim',
+    dependencies = {
+      'leafo/magick',
+    },
+    config = function()
+      require('image').setup {
+        backend = 'kitty',
+        integrations = {
+          markdown = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = true,
+            only_render_image_at_cursor = false,
+            filetypes = { 'markdown', 'vimwiki' }, -- markdown extensions (ie. quarto) can go here
+          },
+        },
+      }
+    end,
+  },
 }
